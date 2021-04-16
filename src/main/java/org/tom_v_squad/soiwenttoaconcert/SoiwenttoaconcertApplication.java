8ee9e41a46a3dd7057e29df8cc302e81135f4810
EKCodeLab <eric@koehn.net>
2021-04-16 14:21:43 -0500

    rebased branch after initial commit and helloworld commit

---
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/SoiwenttoaconcertApplication.java
@@ -0,0 +1,15 @@
+package org.tom_v_squad.soiwenttoaconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoiwenttoaconcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoiwenttoaconcertApplication.class, args);
+//		System.out.println("Hello, World");
+
+	}
+
+}
