23fe24c8a083644ccc14cbae3c555f974df8e88d
EKCodeLab <eric@koehn.net>
2021-04-16 20:59:30 -0500

    rebased branch after initial commit and helloworld commit

---
+++ src/test/java/org/tom_v_squad/soiwenttoaconcert/SoiwenttoaconcertApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tom_v_squad.soiwenttoaconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoiwenttoaconcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
