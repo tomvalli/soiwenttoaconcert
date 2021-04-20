ae1f9509b7f1c2cda866c78096866c17f851cb74
abroksa <slipstitched@gmail.com>
2021-04-20 11:53:31 -0500

    Created Home Controller

---
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.tom_v_squad.soiwenttoaconcert.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("index")
+    public String indexMethod(){
+        return "index";
+    }
+}
