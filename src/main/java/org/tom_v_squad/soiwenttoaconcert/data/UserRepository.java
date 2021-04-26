79dcb29be58ccb56d64d7a80240d59d058d70f05
MarianaES <mariana.enriquez.s@gmail.com>
2021-04-26 14:37:29 -0500

    Create the User Model

--- src/main/java/org/tom_v_squad/soiwenttoaconcert/data/UserRepository.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/data/UserRepository.java
@@ -1,2 +1,8 @@
-package org.tom_v_squad.soiwenttoaconcert.data;public interface UserRepository {
+package org.tom_v_squad.soiwenttoaconcert.data;
+
+import org.springframework.data.repository.CrudRepository;
+import org.tom_v_squad.soiwenttoaconcert.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByUsername(String username);
 }
--- src/main/java/org/tom_v_squad/soiwenttoaconcert/models/User.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/models/User.java
@@ -1,2 +1,44 @@
-package org.tom_v_squad.soiwenttoaconcert.models;public class User {
+package org.tom_v_squad.soiwenttoaconcert.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
 }
