2936f2b1d1351418efcd458a101bd666d8cf4829
abroksa <slipstitched@gmail.com>
2021-04-26 20:24:30 -0500

    blank constructor in event class

--- src/main/java/org/tom_v_squad/soiwenttoaconcert/models/Event.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/models/Event.java
@@ -4,9 +4,9 @@ public class Event {
 
     private String name;
 
-    public Event(String name) {
-        this.name = name;
-    }
+    public Event() { }
+
+
 
     public String getName() {
         return name;
