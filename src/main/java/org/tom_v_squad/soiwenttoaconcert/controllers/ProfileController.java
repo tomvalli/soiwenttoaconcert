17cbe3116c477ba9f115c04e089a7505d74ebc4e
EKCodeLab <eric@koehn.net>
2021-05-16 00:04:44 -0500

    profile/index and profile/edit display static html

--- src/main/java/org/tom_v_squad/soiwenttoaconcert/controllers/ProfileController.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/controllers/ProfileController.java
@@ -22,87 +22,100 @@ import java.util.Optional;
 @Controller
 @RequestMapping("profile")
 public class ProfileController {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    private static final String userSessionKey = "user";
-    private User updateProfile;
-
-
-    public User getUserFromSession(HttpSession session) {
-
-        Integer userId = (Integer) session.getAttribute(userSessionKey);
-        if (userId == null) {
-            return null;
-        }
-
-        Optional<User> user = userRepository.findById(userId);
-
-        if (user.isEmpty()) {
-            return null;
-        }
-        return user.get();
-
-    }
-
-        public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping("profile")
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    private static final String userSessionKey = "user";
+//    private User updateProfile;
+//
+//
+//    public User getUserFromSession(HttpSession session) {
+//
+//        Integer userId = (Integer) session.getAttribute(userSessionKey);
+//        if (userId == null) {
+//            return null;
+//        }
+//
+//        Optional<User> user = userRepository.findById(userId);
+//
+//        if (user.isEmpty()) {
+//            return null;
+//        }
+//        return user.get();
+//
+//    }
+//
+//        public void setUserRepository(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+    @GetMapping("")
     public String displayMyProfile(HttpServletRequest request, Model model) {
-        User user = getUserFromSession(request.getSession());
-        String username = user.getUsername();
-        model.addAttribute("greeting", username);
+//        User user = getUserFromSession(request.getSession());
+//        String username = user.getUsername();
+//        model.addAttribute("greeting", username);
         return "profile/index";
     }
-
-        @PostMapping("edit")
+//
+            @GetMapping("edit")
         public String displayUpdateMyProfile(@ModelAttribute Event updateProfile,
                                                 Errors errors, Model model) {
-            if (errors.hasErrors()) {
-                model.addAttribute("title", "Create Event");
+//            if (errors.hasErrors()) {
+//                model.addAttribute("title", "Create Event");
+//            return "profile/edit";
+//        }
+//            userRepository.save(updateProfile);
             return "profile/edit";
-        }
-            userRepository.save(updateProfile);
-            return "profile/edit";
     }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @GetMapping("edit")
-    public String createProfileForm(Model model) {
-        model.addAttribute("title", "Create Profile");
-        model.addAttribute(new User());
-        return "profile/create";
-    }
-
-    @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Profile newProfile,
-                                         Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Create Profile");
-            return "profile/create";
-        }
-
-        User newUser = null;
-        userRepository.save(newUser);
-        return "profile/create";
-    }
+//        @PostMapping("edit")
+//        public String displayUpdateMyProfile(@ModelAttribute Event updateProfile,
+//                                                Errors errors, Model model) {
+//            if (errors.hasErrors()) {
+//                model.addAttribute("title", "Create Event");
+//            return "profile/edit";
+//        }
+//            userRepository.save(updateProfile);
+//            return "profile/edit";
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    @GetMapping("edit")
+//    public String createProfileForm(Model model) {
+//        model.addAttribute("title", "Create Profile");
+//        model.addAttribute(new User());
+//        return "profile/create";
+//    }
+//
+//    @PostMapping("create")
+//    public String processCreateEventForm(@ModelAttribute Profile newProfile,
+//                                         Errors errors, Model model) {
+//        if (errors.hasErrors()) {
+//            model.addAttribute("title", "Create Profile");
+//            return "profile/create";
+//        }
+//
+//        User newUser = null;
+//        userRepository.save(newUser);
+//        return "profile/create";
+//    }
 
 //    @GetMapping("index")
 //    public String displayEventList(Model model) {
