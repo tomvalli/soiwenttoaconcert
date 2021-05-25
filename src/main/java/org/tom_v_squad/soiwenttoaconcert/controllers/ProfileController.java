b178cdcfc0286487fc97b00261599d85dccad61a
EKCodeLab <eric@koehn.net>
2021-05-25 15:02:23 -0500

    can view profile form and edit username

--- src/main/java/org/tom_v_squad/soiwenttoaconcert/controllers/ProfileController.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/controllers/ProfileController.java
@@ -1,19 +1,64 @@
 package org.tom_v_squad.soiwenttoaconcert.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.tom_v_squad.soiwenttoaconcert.data.UserRepository;
+import org.tom_v_squad.soiwenttoaconcert.models.User;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("profile")
 public class ProfileController {
-    @GetMapping("dog")
+
+    // GETTING THE USER AUTHENTICATED NAME FOR THE PROFILE
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+    private User updateProfile;
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            return null;
+        }
+        return user.get();
+    }
+
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @GetMapping("update")
+    @ResponseStatus //(value=HttpStatus.OK)
     @ResponseBody
-    public String barkWithNoName () {
+    public String updateProfile(HttpServletRequest request) {
+        User user = getUserFromSession(request.getSession());
+        String username = user.getUsername();
+//        String newUsername = user.setUsername(String username);
+        String biography = user.getUserBiography();
+        String location = user.getUserLocation();
+
         return "<html>" +
                 "<body>" +
-                "<h2>My Dog Page</h2>" +
+                "<h2>PROFILE</h2>" +
+
                 "<form action='update' method='post'>" +
-                "<input type='text' name='dogName' />" +
+                "<p>Name:</p>" + username +
+                "<input type='text' name='username' />" +
+                "<p>Bio:</p>"+ biography +
+                "<input type='text' name='biography' />" +
+                "<p>Location:</p>"+ location +
+                "<input type='text' name='location' />" +
                 "<input type='submit' value='update'/>" +
                 "</form>" +
                 "</body>" +
@@ -22,7 +67,9 @@ public class ProfileController {
 
     @PostMapping("update")
     @ResponseBody
-    public String updateTheDog(@RequestParam String dogName) {
-        return "<html><body><h2>" + dogName + " was added!</h2></body></html>";
+    public String updateProfile(@RequestParam String username) {
+
+        return "<html><body><h2>" + username + " was added!</h2></body></html>";
     }
+//    return redirect:../
 }
