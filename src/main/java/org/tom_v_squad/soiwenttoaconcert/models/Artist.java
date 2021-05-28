c2c6039209ef898dab5f3e649e3d2ca66cde9717
EKCodeLab <eric@koehn.net>
2021-05-27 22:54:04 -0500

    annotation error

--- src/main/java/org/tom_v_squad/soiwenttoaconcert/models/Artist.java
+++ src/main/java/org/tom_v_squad/soiwenttoaconcert/models/Artist.java
@@ -11,16 +11,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+
 @Entity
 public class Artist {
-
 
     @Id
     @GeneratedValue
     private int id;
-
 //    @NotNull(message = "Name may not be blank!")
     private String name;
+    private String description;
+    @ManyToMany(mappedBy = "artists")
+    private final List<Event> concerts = new ArrayList<>();
+
+    public Artist() { }
+
+    public Artist(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public int getId() {
         return id;
@@ -34,45 +50,22 @@ public class Artist {
         this.name = name;
     }
 
-//    @Override
-//    public String toString() {
-//        return name;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Artist that = (Artist) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-
-
-//    @NotNull
-//    @Size(min = 1, max = 100, message = "Description must be 1-100 characters long")
-    private String description;
-
-    @ManyToMany(mappedBy = "artists")
-    private final List<Event> concerts = new ArrayList<>();
-
-    public Artist() {
+    @Override
+    public String toString() {
+        return name;
     }
 
-    public Artist(String description) {
-        this.description = description;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist that = (Artist) o;
+        return id == that.id;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
 
